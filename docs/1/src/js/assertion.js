6e890b8049d89ba9c4aa17fe4f52565f4b6ffa67
ytyaru <yttry0@gmail.com>
2026-03-22 18:43:23 +0900

    add

--- docs/1/src/js/assertion.js
+++ docs/1/src/js/assertion.js
@@ -13,17 +13,40 @@ class Assertion {
     assert(a) {throw new ImplementationError(`Assertion.assert(a)をオーバーライドしてください。`);}
 }
 class TypeAssertion extends Assertion {
+    static throw(msg, cause) {throw new TypeAssertionError(msg, cause)}
     constructor(v) {super(v)}
     assert(a) {throw new ImplementationError(`TypeAssertion.assert(a)をオーバーライドしてください。`);}
+    get p() {return new PrimitiveTypeAssertion(this._.v)}
+    get r() {return new ReferenceTypeAssertion(this._.v)}
 }
 class PrimitiveTypeAssertion extends TypeAssertion {
+    static is(v) {return (null===this._.v || ['object','function'].every(n=>n!==typeof this._.v))}
     constructor(v) {super(v)}
     assert(a) {
-        if (null===this._.v || ['object','function'].every(n=>typeof this._.v !==n));
-function isPrimitive(value) {
-  // null と undefined, symbol, bigint, number, string, boolean をカバー
-  return null===vvalue === null || (typeof value !== 'object' && typeof value !== 'function');
-}
-        throw new ImplementationError(`TypeAssertion.assert(a)をオーバーライドしてください。`);
+        if (!PrimitiveTypeAssertion.is(this._.v)) {TypeAssertion.throw(`プリミティブ型ではありません。リファレンス型です。`);}
     }
+    get und() {return new UndefinedTypeAssertion(this._.v)}
+    get nul() {return new NullTypeAssertion(this._.v)}
+    get bln() {return new BooleanTypeAssertion(this._.v)}
+    get num() {return new NumberTypeAssertion(this._.v)}
+    get int() {return new BigIntTypeAssertion(this._.v)}
+    get str() {return new StringTypeAssertion(this._.v)}
+    get sym() {return new SymbolTypeAssertion(this._.v)}
 }
+class ReferenceTypeAssertion extends TypeAssertion {
+    static is(v) {return !PrimitiveTypeAssertion.is(v)}
+    constructor(v) {super(v)}
+    assert(a) {
+        if (!PrimitiveTypeAssertion.is(this._.v)) {TypeAssertion.throw(`リファレンス型ではありません。プリミティブ型です。`);}
+    }
+    get exe() {return new ExecutableTypeAssertion(this._.v)}
+    get ctn() {return new ContainerTypeAssertion(this._.v)}
+    get cls() {return new ClassTypeAssertion(this._.v)}
+    get ins() {return new InstanceTypeAssertion(this._.v)}
+}
+// --------------------------------
+// プリミティブ型
+// --------------------------------
+// --------------------------------
+// リファレンス型
+// --------------------------------
